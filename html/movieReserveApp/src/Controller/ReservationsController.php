f677bf94fb6a845b49e77e9350a333d71722d826
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-10-23 01:16:41 +0900

    addページが読み出された直後に有効期限切れのデータを無効にする処理を追加した。インデントも調整した。

--- html/movieReserveApp/src/Controller/ReservationsController.php
+++ html/movieReserveApp/src/Controller/ReservationsController.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
+use DateTime;
+use Exception;
 
 /**
  * Reservations Controller
@@ -12,130 +15,162 @@ use App\Controller\AppController;
  */
 class ReservationsController extends AppController
 {
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null
-     */
+  /**
+   * Index method
+   *
+   * @return \Cake\Http\Response|null
+   */
 
-    public function initialize()
-    {
-        $title = 'QUEL CINNEMAS';
-        $login = 'ログイン';
+  public function initialize()
+  {
+    $title = 'QUEL CINNEMAS';
+    $login = 'ログイン';
 
-        $this->set(compact('title', 'login'));
-    }
-    public function index()
-    {
-        $this->paginate = [
-            'contain' => ['Schedules', 'Users', 'RegularPrices', 'Discounts'],
-        ];
-        $reservations = $this->paginate($this->Reservations);
+    $this->set(compact('title', 'login'));
+  }
+  public function index()
+  {
+    $this->paginate = [
+      'contain' => ['Schedules', 'Users', 'RegularPrices', 'Discounts'],
+    ];
+    $reservations = $this->paginate($this->Reservations);
 
-        $this->set(compact('reservations'));
-    }
+    $this->set(compact('reservations'));
+  }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Reservation id.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $reservation = $this->Reservations->get($id, [
-            'contain' => ['Schedules', 'Users', 'RegularPrices', 'Discounts'],
-        ]);
+  /**
+   * View method
+   *
+   * @param string|null $id Reservation id.
+   * @return \Cake\Http\Response|null
+   * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+   */
+  public function view($id = null)
+  {
+    $reservation = $this->Reservations->get($id, [
+      'contain' => ['Schedules', 'Users', 'RegularPrices', 'Discounts'],
+    ]);
 
-        $this->set('reservation', $reservation);
-    }
+    $this->set('reservation', $reservation);
+  }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $reservation = $this->Reservations->newEntity();
-        if ($this->request->is('post')) {
-            $reservation = $this->Reservations->patchEntity($reservation, $this->request->getData());
-            if ($this->Reservations->save($reservation)) {
-                $this->Flash->success(__('The reservation has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The reservation could not be saved. Please, try again.'));
-        }
-        $schedules = $this->Reservations->Schedules->find('list', ['limit' => 200]);
-        $users = $this->Reservations->Users->find('list', ['limit' => 200]);
-        $regularPrices = $this->Reservations->RegularPrices->find('list', ['limit' => 200]);
-        $discounts = $this->Reservations->Discounts->find('list', ['limit' => 200]);
-        $schedule_id = $this->request->query['schedule_id']; 
-        // 座席予約をしようとしている上映回に紐づく予約情報をDBから取得
-        $reservations = $this->Reservations->find()->select('seat_number')->where([
-            'schedule_id'=>$schedule_id, 'is_cancelled'=>false, 'is_deleted'=>false
-            ]);
-        // 各予約情報から座席番号を取得し配列 $reservedSeats に代入
-        foreach ($reservations as $reservation) {
-            $reservedSeats[] = $reservation['seat_number'];
-        }
-        $columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
-        $columns_length = sizeof($columns);
-        $rows = 8;
-
-        $this->set(compact(
-            'reservation', 'schedules', 'users', 'regularPrices', 'columns',
-            'columns_length', 'rows', 'discounts', 'reservedSeats'
-        ));
+  /**
+   * Add method
+   *
+   * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+   */
+  public function add()
+  {
+    $current_time = new DateTime();
+    $allCancelled = $this->Reservations->find('all', [
+        'conditions' => array(
+            'expire_at <' => $current_time,
+            'is_cancelled' => false
+        )
+    ]);
+    foreach ($allCancelled as $cancelled) {
+        $cancelled->is_cancelled = true;
+        $this->Reservations->save($cancelled);
     }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Reservation id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $reservation = $this->Reservations->get($id, [
-            'contain' => [],
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $reservation = $this->Reservations->patchEntity($reservation, $this->request->getData());
-            if ($this->Reservations->save($reservation)) {
-                $this->Flash->success(__('The reservation has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The reservation could not be saved. Please, try again.'));
-        }
-        $schedules = $this->Reservations->Schedules->find('list', ['limit' => 200]);
-        $users = $this->Reservations->Users->find('list', ['limit' => 200]);
-        $regularPrices = $this->Reservations->RegularPrices->find('list', ['limit' => 200]);
-        $discounts = $this->Reservations->Discounts->find('list', ['limit' => 200]);
-        $this->set(compact('reservation', 'schedules', 'users', 'regularPrices', 'discounts'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Reservation id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $reservation = $this->Reservations->get($id);
-        if ($this->Reservations->delete($reservation)) {
-            $this->Flash->success(__('The reservation has been deleted.'));
-        } else {
-            $this->Flash->error(__('The reservation could not be deleted. Please, try again.'));
-        }
+    $reservation = $this->Reservations->newEntity();
+    if ($this->request->is('post')) {
+      $reservation = $this->Reservations->patchEntity($reservation, $this->request->getData());
+      if ($this->Reservations->save($reservation)) {
+        $this->Flash->success(__('The reservation has been saved.'));
 
         return $this->redirect(['action' => 'index']);
+      }
+      $this->Flash->error(__('The reservation could not be saved. Please, try again.'));
     }
+    $schedules = $this->Reservations->Schedules->find('list', ['limit' => 200]);
+    $users = $this->Reservations->Users->find('list', ['limit' => 200]);
+    $regularPrices = $this->Reservations->RegularPrices->find('list', ['limit' => 200]);
+    $discounts = $this->Reservations->Discounts->find('list', ['limit' => 200]);
+    $schedule_id = $this->request->query['schedule_id'];
+    // 座席予約をしようとしている上映回に紐づく予約情報をDBから取得
+    try {
+      $reservations = $this->Reservations->find()
+        ->select('seat_number')
+        ->where([
+          'schedule_id' => $schedule_id,
+          'is_cancelled' => false,
+          'is_deleted' => false
+        ]);
+    } catch (Exception $e) {
+      $reservations = null;
+    }
+    // 各予約情報から座席番号を取得し配列$reservedSeatsに代入
+    if ($reservations) {
+      foreach ($reservations as $reservation) {
+        $reservedSeats[] = $reservation['seat_number'];
+      }
+    } elseif ($reservation === null) {
+      $reservedSeats = [];
+    }
+    $columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
+    $columns_length = sizeof($columns);
+    $rows = 8;
+
+    $this->set(compact(
+      'reservation',
+      'schedules',
+      'users',
+      'regularPrices',
+      'columns',
+      'columns_length',
+      'rows',
+      'discounts',
+      'reservedSeats',
+      'reservations',
+    ));
+  }
+
+  /**
+   * Edit method
+   *
+   * @param string|null $id Reservation id.
+   * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+   * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+   */
+  public function edit($id = null)
+  {
+    $reservation = $this->Reservations->get($id, [
+      'contain' => [],
+    ]);
+    if ($this->request->is(['patch', 'post', 'put'])) {
+      $reservation = $this->Reservations->patchEntity($reservation, $this->request->getData());
+      if ($this->Reservations->save($reservation)) {
+        $this->Flash->success(__('The reservation has been saved.'));
+
+        return $this->redirect(['action' => 'index']);
+      }
+      $this->Flash->error(__('The reservation could not be saved. Please, try again.'));
+    }
+    $schedules = $this->Reservations->Schedules->find('list', ['limit' => 200]);
+    $users = $this->Reservations->Users->find('list', ['limit' => 200]);
+    $regularPrices = $this->Reservations->RegularPrices->find('list', ['limit' => 200]);
+    $discounts = $this->Reservations->Discounts->find('list', ['limit' => 200]);
+    $this->set(compact('reservation', 'schedules', 'users', 'regularPrices', 'discounts'));
+  }
+
+  /**
+   * Delete method
+   *
+   * @param string|null $id Reservation id.
+   * @return \Cake\Http\Response|null Redirects to index.
+   * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+   */
+  public function delete($id = null)
+  {
+    $this->request->allowMethod(['post', 'delete']);
+    $reservation = $this->Reservations->get($id);
+    if ($this->Reservations->delete($reservation)) {
+      $this->Flash->success(__('The reservation has been deleted.'));
+    } else {
+      $this->Flash->error(__('The reservation could not be deleted. Please, try again.'));
+    }
+
+    return $this->redirect(['action' => 'index']);
+  }
 }
--- html/movieReserveApp/src/Template/Reservations/add.ctp
+++ html/movieReserveApp/src/Template/Reservations/add.ctp
@@ -13,12 +13,15 @@
         <li>8</li>
       </ul>
       <?php
+        // 縦の列数だけ繰り返し処理で描画を行う
         for ($i = 0; $i < $columns_length; $i++) {
           $column = $columns[$i];
           echo "<ul class='seats-columns'>";
           echo "<li>{$column}</li>";
+          // 横の列数だけ繰り返し処理で描画を行う
           for ($j = 1; $j <= $rows; $j++) {
             $seat_id = $column . $j;
+            // 描画する席が予約済みの場合はクラス「unavailable」を付与し表示を変える
             if (in_array($seat_id, $reservedSeats)) {
               echo "<li class='unavailable' id='{$seat_id}'>";
             } else {
