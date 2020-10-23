bf5dc9fb38d6a759e1771adc492a8506c07bb1aa
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-10-23 23:34:06 +0900

    関数setSeatNumber()を定義し、コードを簡略化。

--- html/movieReserveApp/webroot/js/reservations.js
+++ html/movieReserveApp/webroot/js/reservations.js
@@ -2,22 +2,28 @@
 
 $(function() {
 
+  function setSeatNumber() {
+    var selected = document.getElementsByClassName('selected');
+    var selected_seat = selected[0].id;
+
+    document.getElementById('seat-number').setAttribute('value', selected_seat);
+  }
+
   $(document).on('click', '.available', function() {
     $('.selected').toggleClass('selected');
     $(this).addClass('selected');
+
+    setSeatNumber();
   });
 
   $(document).on('click', '.selected', function() {
     if ($(this).hasClass('selected')) {
       $(this).removeClass('selected');
+
+      document.getElementById('seat-number').setAttribute('value', '');
     }
+
+    setSeatNumber();
   })
 
-  $(document).on('click', '#confirm-btn', function() {
-    var selected = document.getElementsByClassName('selected');
-    var selected_seat = selected[0].id;
-
-    document.getElementById('seat-number').setAttribute('value', selected_seat);
-
-  })
 });
