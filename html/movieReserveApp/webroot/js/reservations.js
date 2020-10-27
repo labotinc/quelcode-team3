623db7a42ac750c28ac4eb9012189045f398bfb7
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-10-27 15:57:21 +0900

    jsファイルにコメントを添えた。

--- html/movieReserveApp/webroot/js/reservations.js
+++ html/movieReserveApp/webroot/js/reservations.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(function() {
-
+  // 座席番号のデータを保持する処理
   function setSeatNumber() {
     var selected = document.getElementsByClassName('selected');
     var selected_seat = selected[0].id;
@@ -9,9 +9,10 @@ $(function() {
     document.getElementById('seat-number').setAttribute('value', selected_seat);
   }
 
+  // 選択可能な座席を押下した際の処理
   $(document).on('click', '.available', function() {
-    // 座席選択後に別の席をクリックした際の処理
     if (!$(this).hasClass('selected')) {
+      // 既に押下済みの状態で別の座席を選択した場合は、押下済みの選択が外れて新しく押下した座席を選択したこととする 
       $('.selected').removeClass('selected');
       $(this).addClass('selected');
 
@@ -21,6 +22,7 @@ $(function() {
 
   $(document).on('click', '.selected', function() {
     if ($(this).hasClass('selected')) {
+      // 選択済みの座席を再度押下した場合は、選択を解除し座席情報のデータも空にする
       $(this).removeClass('selected');
 
       document.getElementById('seat-number').setAttribute('value', '');
