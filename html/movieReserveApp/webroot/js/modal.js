06ca1da697b86be5b072e9faf95337f6a805d2cd
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2020-11-16 16:00:57 +0900

    キャンセルボタン押下時にinputタグのvalueを設定する。

--- html/movieReserveApp/webroot/js/modal.js
+++ html/movieReserveApp/webroot/js/modal.js
@@ -3,6 +3,8 @@
 $(function () {
   $('.reservations-cancel-button').click(function () {
     $('#modalArea').fadeIn();
+    const reservation_id = $(this)[0].id;
+    document.getElementById('reservation_id').setAttribute('value', reservation_id);
   });
   $('#closeModal, #modalBg, .abort').click(function () {
     $('#modalArea').fadeOut();
